f5045645570f2677aa8ab7c82cc473ccca95aeb0
anees-kait <anees@kaitsolutions.com>
2021-10-25 03:39:28 +0300

    email fix

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'adminEmail' => 'mohammad.riad@gmail.com',
-    'supportEmail' => 'mohammad.riad@gmail.com',
+    'adminEmail' => 'yii2Test@yii2Test.com',
+    'supportEmail' => 'yii2Test@yii2Test.com',
     'user.passwordResetTokenExpire' => 3600,
     'user.rememberMeDuration' => 2592000,
     'googleMapKey' => 'MAP_KEY_HERE',
